86c135a700fd854d4f2ca5e243087d1a1e504ec2
litzzz <3246296691@qq.com>
2026-07-29 10:29:48 +0800

    关闭 IMU 模块

--- E5_04_ips200_display_demo/code/module_config.h
+++ E5_04_ips200_display_demo/code/module_config.h
@@ -8,7 +8,7 @@
 
 #define ENABLE_MOTOR        1       // 电机模块
 #define ENABLE_GRAYSCALE    1       // 灰度传感器模块
-#define ENABLE_IMU          1       // IMU660RB 姿态传感器模块
+#define ENABLE_IMU          0       // IMU660RB 姿态传感器模块
 #define ENABLE_TRACKING     1       // 循迹模块 (依赖电机+灰度+IMU)
 #define ENABLE_LASER        0       // 激光模块
 
